ae18424c794c9f1e40141fe1e4cf81fe47d16dc2
Tolokov <vitaliitolokov@gmail.com>
2021-12-17 13:29:16 +0500

    create firts get request

--- Shop/views.py
+++ Shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from django.views.generic import View
 
 
-
+class PostView(View):
+    def get(self, request):
+        return render(request, 'base.html')
