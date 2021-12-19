8f3fef334af13500d686aa4a1474965af072918b
Tolokov <vitaliitolokov@gmail.com>
2021-12-19 16:07:23 +0500

    create models Category/Brand/Cart_Product/ProductImage/Rating

--- Shop/models.py
+++ Shop/models.py
@@ -76,45 +76,126 @@ class Appeal_to_support(models.Model):
 
 
 
+# step 2
+class Category(models.Model):
+    '''
+    Многие ко многим
+    '''
+    name = models.CharField("Категория", max_length=150)
+    description = models.TextField("Описание", max_length=1500)
+    url = models.SlugField(max_length=160, unique=True)
 
-#
-# # step 2
-# class Card_Product(models.Model):
-#     '''
-#     Карточка продукта
-#     '''
-#
-#
-# class Category(models.Model):
-#     '''
-#     Многие ко многим
-#     '''
-#
-#
-# class Brand(models.Model):
-#     '''
-#     Один ко многим
-#     '''
-#
-#
-# class Product_Image(models.Model):
-#     '''
-#     Изображения отображаемые с карточкой товара.
-#     '''
-#
-#
-# class Rating(models.Model):
-#     '''
-#     Рейтинг выставленный продукту
-#     '''
-#
-#
-# class Rating_star(models.Model):
-#     '''
-#     Отображаемые значения рейтинга
-#     '''
-#
-#
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+class Brand(models.Model):
+    '''
+    Один ко многим
+    '''
+    name = models.CharField("Категория", max_length=150)
+    description = models.TextField("Описание", max_length=1500)
+    url = models.SlugField(max_length=160, unique=True)
+    logo = ImageField("Логотип", upload_to="media/brands/")
+
+    def __str__(self):
+        return self.name
+
+
+class Cart_Product(models.Model):
+    '''
+    Карточка продукта
+    '''
+    product_public_ID = models.IntegerField(blank=False, primary_key=False, max_length=6, default=100000)
+    # product_public_ID = models.AutoField(
+    #     auto_created=True,
+    #     primary_key=False,
+    #     blank=False,
+    #     max_length=6,
+    #     default=100000,
+    # )
+    name = CharField(max_length=300)
+    description = TextField(max_length=5000)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    availability = BooleanField(default=True, verbose_name='hidden')
+
+    NEW = 'N'
+    FRESH = 'F'
+    SECOND_HAND = 'S'
+    HIDDEN = 'H'
+    CONDITION_CHOICE = [
+        (NEW, 'new'),
+        (FRESH, 'fresh'),
+        (SECOND_HAND, 'second hand'),
+    ]
+    condition = models.CharField(
+        max_length=1,
+        choices=CONDITION_CHOICE,
+        default=FRESH,
+        blank=False,
+    )
+
+    icon = ImageField('Изображение в корзине', upload_to='media/product_icon')
+    category = models.ManyToManyField(Category, verbose_name="категории")
+    brand = models.ForeignKey(Brand, verbose_name="бренды", on_delete = models.SET_NULL, null = True, blank=False)
+
+    def __str__(self):
+        return f'ID: {self.product_public_ID} NAME: {self.name}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = ' Товары'
+
+
+
+
+
+class ProductImage(models.Model):
+    '''
+    Изображения отображаемые с карточкой товара.
+    '''
+    title = models.CharField("Заголовок", max_length=100)
+    description = models.TextField("Описание", max_length=2000)
+    image = models.ImageField("Фотография товара", upload_to="media/")
+    product = models.ForeignKey(Cart_Product, verbose_name="Фотографии", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Фотография'
+        verbose_name_plural = 'Фотографии'
+
+class RatingGrade(models.Model):
+    '''
+    Отображаемые значения рейтинга
+    '''
+    value = models.SmallIntegerField("Рейтинг", default=0)
+
+    def __str__(self):
+        return f' Текущая оценка продукта{self.value}'
+
+
+class Rating(models.Model):
+    '''
+    Рейтинг выставленный продукту
+    '''
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="создатель")
+    grade = models.ForeignKey(RatingGrade, on_delete=models.CASCADE, verbose_name="оценка")
+    product = models.ForeignKey(Cart_Product, on_delete=models.CASCADE, verbose_name="продукт")
+
+    def __str__(self):
+        return f'{self.product}, {self.grade}, {self.creator}'
+
+    class Meta:
+        verbose_name = "Рейтинг"
+        verbose_name_plural = "Рейтинги"
+
+
+
 # # step 3
 # class Cart(models.Model):
 #     '''
