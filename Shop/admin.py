2c8b88320bad79bf17f1d399a5424ffe2fb42937
Tolokov <vitaliitolokov@gmail.com>
2021-12-21 12:35:30 +0500

    register new models

--- Shop/admin.py
+++ Shop/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import News, Comment, Appeal_to_support, Category, Brand, Cart_Product
-from .models import ProductImage, RatingGrade, Rating
+from .models import News, Comment, Appeal_to_support, Category, Brand, Card_Product, Order
+from .models import ProductImage, RatingGrade, Rating, Favorites, Cart, CartProduct
 # Register your models here.
 
 admin.site.register(News)
@@ -9,10 +9,15 @@ admin.site.register(Comment)
 admin.site.register(Appeal_to_support)
 admin.site.register(Category)
 admin.site.register(Brand)
-admin.site.register(Cart_Product)
+admin.site.register(Card_Product)
 admin.site.register(ProductImage)
 admin.site.register(RatingGrade)
 admin.site.register(Rating)
+
+admin.site.register(Favorites)
+admin.site.register(Cart)
+admin.site.register(CartProduct)
+admin.site.register(Order)
 
 
 
