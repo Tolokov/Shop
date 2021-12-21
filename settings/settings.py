abe6b341a9867146b04800e74bc378a7e321eae8
Tolokov <vitaliitolokov@gmail.com>
2021-12-21 14:41:40 +0500

    created login

--- settings/settings.py
+++ settings/settings.py
@@ -87,7 +87,10 @@ USE_TZ = True
 
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 # LOGIN_URL = 'registration/login/'
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 STATIC_URL = 'static/'
