5615b51553a7c669a085f528a5e757cc2b8e4cc3
Tolokov <vitaliitolokov@gmail.com>
2021-12-24 13:00:39 +0500

    update ex404()

--- Shop/views.py
+++ Shop/views.py
@@ -4,15 +4,14 @@ from django.views.generic import View
 from .models import News, Card_Product, Category
 
 
-# def ex404(request, exception):
-#     context = {}
-#     context['page_title'] = '404'
-#     response = render(request, 'exception/404.html', status=404)
-#     response.status_code = 404
-#     return response
-
 def ex404(request, exception):
-    return render(request, 'exception/404.html', status=404)
+    context = {'errorMessage': 'We Couldn’t Find this Page'}
+    print(exception)
+    if isinstance(exception, int):
+        context['errorMessage'] = 'Новость не существует'
+    request = render(request, 'exception/404.html', status=404, context=context)
+    return request
+
 
 class HomeView(View):
     def get(self, request):
@@ -21,25 +20,26 @@ class HomeView(View):
             'products': products,
         })
 
+
 class ProductDetailView(View):
     def get(self, request, product_public_ID):
         product_detail = Card_Product.objects.get(product_public_ID=product_public_ID)
         return render(request, 'pages/product-detail.html', {'product_detail': product_detail})
+
 
 class BlogListView(View):
     def get(self, request):
         posts = News.objects.filter(draft=False)
         return render(request, 'pages/blog.html', {'posts': posts})
 
+
 class SinglePostVies(View):
     def get(self, request, post_id):
         single_post = News.objects.get(id=post_id)
-        return render(request, 'pages/blog-single.html', {'single_post': single_post})
-
-
-
-
-
+        if single_post.draft:
+            return ex404(request, exception=post_id)
+        else:
+            return render(request, 'pages/blog-single.html', {'single_post': single_post})
 
 
 # /////////////////////////////
@@ -48,29 +48,22 @@ class CartView(View):
     def get(self, request):
         return render(request, 'pages/cart.html', {})
 
+
 class CheckoutView(View):
     def get(self, request):
         return render(request, 'pages/checkout.html', {})
 
+
 class ContactView(View):
     def get(self, request):
         return render(request, 'pages/contact-us.html', {})
+
 
 class ShopView(View):
     def get(self, request):
         return render(request, 'pages/shop.html', {})
 
 
-
-
 class FavoritesView(View):
     def get(self, request):
         return render(request, 'pages/product-detail.html', {})
-
-
-
-
-
-
-
-
