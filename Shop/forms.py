3cb11b00c2c55a01067a31882450d6b3411fa885
Tolokov <vitaliitolokov@gmail.com>
2021-12-27 14:54:46 +0500

    update ContactFormView

--- Shop/forms.py
+++ Shop/forms.py
@@ -22,9 +22,33 @@ class AddNewAddressDeliveryForm(Form):
     fax = CharField(max_length=20)
     comment = CharField()
 
+
 class ContactForm(Form):
+    required = {"required": "required"}
+
+    name_attrs = {
+        "class": "form-control",
+        "type": "text",
+        "name": "name",
+        "placeholder": "Name",
+    } | required
+
+    email_attrs = {
+        "class": "form-group col-md-6 form-control",
+        "type": "email",
+        "name": "email",
+        "placeholder": "Email",
+    } | required
+
+    text_attrs = {
+        "class": "form-group col-md-18 form-control",
+        "name": "message",
+        "id": "message",
+        "rows": "8",
+    } | required
+
     name = CharField()
-    email = CharField()
-    text = CharField(widget=Textarea)
-
-
+    name.widget.attrs.update(name_attrs)
+    email = EmailField()
+    email.widget.attrs.update(email_attrs)
+    text = CharField(widget=Textarea(attrs=text_attrs))
