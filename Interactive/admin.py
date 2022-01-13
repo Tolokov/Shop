25825143c1404bd40a99a81a7ffe58bdfad57d3f
Tolokov <vitaliitolokov@gmail.com>
2022-01-13 18:29:52 +0500

    Output image on admin page

--- Interactive/admin.py
+++ Interactive/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-import Interactive.models
 from Interactive.models import Delivery, Customer
 
 
@@ -11,7 +11,11 @@ class DeliveryAdmin(admin.ModelAdmin):
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('user', 'first_name', 'last_name', 'phone', 'email')
+    list_display = ('user', 'first_name', 'last_name', 'get_avatar')
     save_on_top = True
     search_fields = ('first_name', 'last_name', 'phone', 'email')
     list_filter = ('first_name', 'last_name', 'phone', 'email')
+    readonly_fields = ('get_avatar',)
+
+    def get_avatar(self, obj):
+        return mark_safe(f'<img src={obj.avatar.url} width="110" height="80"')
